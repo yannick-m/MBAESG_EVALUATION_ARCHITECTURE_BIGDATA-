8c7ffa229af50ae34be04e005046add28dbfcde6
mariska djomo <mariskayannedjomo@gmail.com>
2025-07-04 17:44:40 +0200

    ajout création stage

--- src/requete_sql.sql
+++ src/requete_sql.sql
@@ -10,3 +10,8 @@ CREATE OR REPLACE WAREHOUSE linkedin_wh
   AUTO_SUSPEND = 60
   AUTO_RESUME = TRUE;
 USE WAREHOUSE linkedin_wh;
+
+--Créer un stage externe (accès au bucket public S3)
+CREATE OR REPLACE STAGE linkedin_stage
+  URL='s3://snowflake-lab-bucket/'
+  FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1);
